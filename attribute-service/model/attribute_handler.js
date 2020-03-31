a01b7c98f5c3caa68b6a4702279d07cfe480beb9
evan-krul <evan@krul.ca>
2020-03-30 23:23:03 -0600

    Added attribute projection gets

--- attribute-service/model/attribute_handler.js
+++ attribute-service/model/attribute_handler.js
@@ -6,19 +6,22 @@ module.exports = {
         db.getAttributeByCourseId(data.course_id).then(attributes => {
             if(attributes === null) {
                 attributes = new Attribute(data.course_id, data.rating, data.tags);
-                console.log(attributes);
 
                 db.insertNewAttributes(attributes);
             } else {
                 attributes._total++;
                 attributes._rating -= -1*data.rating;
                 attributes._tags = attributes._tags.concat(data.tags);
-                console.log(attributes);
 
                 db.updateAttributeById(attributes._attribute_id, attributes);
             }
         });
-
-
+    },
+    getAttributes: function(course_id) {
+        return new Promise(function (resolve, reject) {
+            db.getAttributeByCourseId(course_id).then(attributes => {
+                resolve(attributes);
+            }).catch(err => reject(err));
+        });
     }
 }
