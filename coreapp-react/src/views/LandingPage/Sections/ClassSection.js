1fe05c12a8da9a43f6620470689c3b09dd7774ed
Nathan Darby <nathan.darby1@ucalgary.ca>
2020-04-02 11:41:19 -0600

    Attribute and Rating fixes

--- coreapp-react/src/views/LandingPage/Sections/ClassSection.js
+++ coreapp-react/src/views/LandingPage/Sections/ClassSection.js
@@ -3,80 +3,19 @@ import { useParams } from "react-router";
 
 // @material-ui/core components
 import {makeStyles} from "@material-ui/core/styles";
-import {withStyles} from "@material-ui/core/styles";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 
 import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
-import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
-import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
-import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
-import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
-import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
-import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Rating from '@material-ui/lab/Rating';
 
 import classNames from "classnames";
 
 import ReviewSection from "./ReviewSection.js";
-
+import AttributeSection from "./AttributeSection.js";
 
 const useStyles = makeStyles(styles);
-const customIcons = {
-    1: {
-        icon: <SentimentVeryDissatisfiedIcon/>,
-        label: 'Very Dissatisfied',
-    },
-    2: {
-        icon: <SentimentDissatisfiedIcon/>,
-        label: 'Dissatisfied',
-    },
-    3: {
-        icon: <SentimentSatisfiedIcon/>,
-        label: 'Neutral',
-    },
-    4: {
-        icon: <SentimentSatisfiedAltIcon/>,
-        label: 'Satisfied',
-    },
-    5: {
-        icon: <SentimentVerySatisfiedIcon/>,
-        label: 'Very Satisfied',
-    },
-};
 
-// const Comment = React.createClass({
-//   render() {
-//     const comment = this.props.comment;
-//     return <div>
-//       <div dangerouslySetInnerHTML={{__html: comment.comment_text}}/>
-//       {comment.children.length > 0 && comment.children.map((child) => {
-//         return <Comment key={child.id} comment={child}/>
-//       })}
-//     </div>
-//   }
-// });
-
-function IconContainer(props) {
-    const {value, ...other} = props;
-    return <span {...other}>{customIcons[value].icon}</span>;
-}
-
-IconContainer.propTypes = {
-    value: PropTypes.number.isRequired,
-};
-
-const StyledRating = withStyles({
-    iconFilled: {
-        color: '#033285',
-    },
-    iconHover: {
-        color: '#033285',
-    },
-})(Rating);
 
 // create function with a get request here to be used and displayed below
 
@@ -103,16 +42,7 @@ export default function ProductSection() {
             </GridContainer>
             <div>
 
-                <Box component="fieldset" mb={3} borderColor="transparent">
-                    <Typography component="legend"> </Typography>
-                    <StyledRating
-                        name="customized-icons"
-                        defaultValue={3} //CHANGE VALUE TO VALUE FROM BACK END HERE
-                        getLabelText={value => customIcons[value].label}
-                        IconContainerComponent={IconContainer}
-                    >Current Course Rating</StyledRating>
-                </Box>
-                {/*reviews displayed here*/}
+                <AttributeSection/>
                 <ReviewSection/>
 
             </div>
